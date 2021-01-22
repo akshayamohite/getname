6f2c8670fb8dbea4a9e9b6a3716290ca03210f93
Akshay Mohite <akshay.m@media.net>
2021-01-22 19:52:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# What it is
+
+Add name and get surname
+
+# Installation
+
+`npm i getnameak --save`
+
+Then...
+
+```
+import { getnameak } from 'getnameak';
+
+getnameak();
+
+```
+
+
---
+++ index.js
@@ -0,0 +1,12 @@
+function getnameak(str){
+	let updatedStr = str.trim()
+	if(updatedStr == 'akshay'){
+		return 'mohite'
+	}
+	else{
+		return 'try one more time'
+	}
+}
+
+
+module.exports.getnameak = getnameak;
